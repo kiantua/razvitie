dc0528bf7bfc6bd2a1980aa870992c7f36681742
kiantua <kiant.ua@gmail.com>
2017-10-08 11:06:43 +0300

    08/10/2017

---
+++ JS/main.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Антон on 07.10.2017.
+ */
+(function () {
+    var buttonHide =  document.querySelector('.hide-admin-login'),
+        buttonShow = document.querySelector('.show-admin-login'),
+        form = document.querySelector('.login-admin');
+
+    function hideAdmin() {
+        form.className = "login-form-wrapper login-admin hidden";
+    }
+
+    function showAdmin() {
+        form.className = "login-form-wrapper login-admin visible";
+    }
+
+
+
+    buttonHide.addEventListener("click", hideAdmin);
+    buttonShow.addEventListener("click", showAdmin);
+
+
+
+}());
